5754a4f17126ec6ae745e73641cb94695c617f36
Chris Blackburn <thetootall@gmail.com>
2019-09-03 15:30:14 -0500

    Update licensebreakdown.ps1

--- licensebreakdown.ps1
+++ licensebreakdown.ps1
@@ -1,5 +1,7 @@
-#Establish a PowerShell session with Office 365. You'll be prompted for your Delegated Admin credentials
 Connect-MsolService
+$listGetMsolAccountSku = Get-MsolAccountSku | Select-Object -property AccountSkuId, ActiveUnits, ConsumedUnits    
+$listGetMsolAccountSku | export-csv totalO365license.csv -NoTypeInformation 
+                                                                                                                  
 $CSVpath = "UserLicenseReport.csv"
   
     $licensedUsers = Get-MsolUser -All | Where-Object {$_.islicensed}
@@ -9,7 +11,7 @@ $CSVpath = "UserLicenseReport.csv"
         $licenses = $user.Licenses
         $licenseArray = $licenses | foreach-Object {$_.AccountSkuId}
         $licenseString = $licenseArray -join ", "
-        Write-Host "$($user.displayname) has $licenseString" -ForegroundColor Blue
+        Write-Host "$($user.displayname) has $licenseString" -ForegroundColor Red
         $licensedSharedMailboxProperties = [pscustomobject][ordered]@{
             DisplayName       = $user.DisplayName
             Licenses          = $licenseString
@@ -17,3 +19,5 @@ $CSVpath = "UserLicenseReport.csv"
         }
         $licensedSharedMailboxProperties | Export-CSV -Path $CSVpath -Append -NoTypeInformation   
 }
+
+Write-host "Export complete; please return UserLicenseReport.csv & totalO365license.csv to consultant" -ForegroundColor Yellow -BackgroundColor Black
